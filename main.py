1c6e91ef34cb399c695fc5546dcb1a77e6857a5f
pshemen <pshemen@gmail.com>
2022-02-15 19:35:01 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+
+# for i in range (4):
+#     plt.plot(np.random.rand(10))
+# plt.show()
+
+
+
